ecf78924cab99faff52162b3d92898de090778c4
Daltimore <obinnaleo78@gmail.com>
2020-04-13 17:50:18 +0100

    init

---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <main id="home-layout">
+    <h1 class="d-flex justify-content-center align-items-center">This is the Home Layout</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <main id="home">
+    <h1 class="d-flex justify-content-center align-items-center"></h1>
+  </main>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
